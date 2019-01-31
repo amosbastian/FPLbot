c0d47a10d63a637867a67ce288640bda12173864
amosbastian <amosbastian@gmail.com>
2019-01-31 17:56:14 +0000

    Add post_price_changes() for Reddit

--- FPLbot/bot.py
+++ FPLbot/bot.py
@@ -1,15 +1,30 @@
 import asyncio
+import json
+import os
+from datetime import datetime
 
 import aiohttp
+import praw
 from fpl import FPL
+from fpl.utils import position_converter
 from pymongo import MongoClient
+
+from utils import get_player_table
+
+dirname = os.path.dirname(os.path.realpath(__file__))
 
 
 class FPLBot:
     def __init__(self, config, session):
         self.client = MongoClient()
-        self.config = config
         self.fpl = FPL(session)
+        self.reddit = praw.Reddit(
+            client_id=config.get("CLIENT_ID"),
+            client_secret=config.get("CLIENT_SECRET"),
+            password=config.get("PASSWORD"),
+            user_agent=config.get("USER_AGENT"),
+            username=config.get("USERNAME"))
+        self.subreddit = self.reddit.subreddit(config.get("SUBREDDIT"))
 
     async def get_price_changers(self):
         """Returns a list of players whose price has changed since the last
@@ -32,3 +47,33 @@ class FPLBot:
                     risers.append(new_player)
 
         return risers, fallers
+
+    async def post_price_changes(self):
+        risers, fallers = await self.get_price_changers()
+        risers_table = get_player_table(risers, True)
+        fallers_table = get_player_table(fallers, False)
+
+        post_template = open(f"{dirname}/../post_template.md").read()
+        post_body = post_template.format(
+            risers_number=len(risers),
+            risers_table=risers_table,
+            fallers_number=len(fallers),
+            fallers_table=fallers_table
+        )
+
+        today = datetime.now()
+        current_date = f"({today:%B} {today.day}, {today.year})"
+        post_title = f"Player Price Changes {current_date}"
+        self.subreddit.submit(post_title, selftext=post_body)
+
+
+async def main(config):
+    async with aiohttp.ClientSession() as session:
+        fpl_bot = FPLBot(config, session)
+
+        await fpl_bot.post_price_changes()
+
+
+if __name__ == "__main__":
+    config = json.loads(open(f"{dirname}/../config.json").read())
+    asyncio.run(main(config))
